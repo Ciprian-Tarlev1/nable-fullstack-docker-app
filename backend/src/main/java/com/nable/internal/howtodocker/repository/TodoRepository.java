5b98671e4e081e0ed4e05bdc5c662877bc7f6f20
cipriantarlev <ciprian.tarlev@n-able.com>
2023-10-24 17:59:12 +0300

    Add frontend-service in crossorigin

--- backend/src/main/java/com/nable/internal/howtodocker/repository/TodoRepository.java
+++ backend/src/main/java/com/nable/internal/howtodocker/repository/TodoRepository.java
@@ -4,6 +4,6 @@ import com.nable.internal.howtodocker.repository.entity.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin(origins = { "http://localhost", "http://localhost:3000" })
+@CrossOrigin(origins = { "http://localhost", "http://localhost:3000", "http://frontend-service", "http://frontend-service:3000" })
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
 }
